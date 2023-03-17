c50eb9ba0441474c2efb2c999bd883439df1ea71
Ramlachujbk <ramlachu@jbkinfotech.com>
2023-03-17 15:42:22 +0530

    006 page

---
+++ js/custom-slick.js
@@ -0,0 +1,35 @@
+
+var $ = jQuery.noConflict();
+$(document).ready(function(){
+  
+    $('.full-width-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '200px',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        variableWidth: true,
+        dots: true,
+        arrows: true,
+        prevArrow: '<div class="slick-arrow slick-prev"><span class="slick-btn fa-light fa-angle-left flex flex-center"></span></div>',
+        nextArrow: '<div class="slick-arrow slick-next"><span class="slick-btn fa-light fa-angle-right flex flex-center"></span></div>',
+        responsive: [
+            {
+            breakpoint: 1024,
+                settings: {
+                    centerPadding: '100px',        
+                    variableWidth: false,
+                }
+            },
+            {
+            breakpoint: 767,
+                settings: {
+                    centerPadding: '30px',  
+                    variableWidth: false,
+                }
+            }
+        ]
+    });
+});
+
